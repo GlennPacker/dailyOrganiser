2d4a3fdc81781258c2e2ebeab2f6038428e29348
DESKTOP-ACT9QHL\Glenn <glenn@glennpacker.net>
2019-07-27 12:28:06 +0100

    routing added tasklist task create

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from "../store";
 import { ConnectedDashboard } from "./dashboard";
+import { Router, Route } from 'react-router-dom';
+import {history} from '../store/history';
+import { ConnectedNavigation } from '../components/navigation'
 
 export const Main = () => (
-    <Provider store={store}>
-        <div>
-            <ConnectedDashboard />
-        </div>
-    </Provider>
+    <Router history={history}>
+        <Provider store={store}>
+            <div>
+                <ConnectedNavigation />
+                <h1>Daily Planner</h1>
+                <Route
+                    exact
+                    path="/dashboard"
+                    render={() => <ConnectedDashboard />}
+                />
+            </div>
+        </Provider>
+    </Router>
 )
